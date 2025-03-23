ee49584551dabbce4bffa75180c0a3b49b534999
shubhendu ghosh <shubhendu-ghosh@users.noreply.huggingface.co>
2025-03-23 15:11:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,19 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-# Home route displays the signup page
 @app.route('/')
 def home():
     return render_template('signup.html')
 
-# Login page route
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-# Chat page route
 @app.route('/chat')
 def chat():
     return render_template('index.html')
 
-# Static files (JS/CSS) are served from /static by default.
 
 if __name__ == '__main__':
     app.run(debug=True)
